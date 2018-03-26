10f30fa657f0f802e0985bbf25475f8a4cf478f6
Nicholas Smith <nick74044@outlook.com>
2018-03-26 19:45:21 +0100

    Added file to check answers to each problem

---
+++ TestAnswers.cpp
@@ -0,0 +1,23 @@
+#include "TestAnswers.h"
+using namespace std;
+
+void testAnswers()
+{
+	int i = 1;
+	cout << i++ << ": " << solutionOne() << " | " << (solutionOne() == 233168) << endl;
+	cout << i++ << ": " << solutionTwo() << " | " << (solutionTwo() == 4613732) << endl;
+	cout << i++ << ": " << solutionThree() << " | " << (solutionThree() == 6857) << endl;
+	cout << i++ << ": " << solutionFour() << " | " << (solutionFour() == 906609) << endl;
+	cout << i++ << ": " << solutionFive() << " | " << (solutionFive() == 232792560) << endl;
+	cout << i++ << ": " << solutionSix() << " | " << (solutionSix() == 25164150) << endl;
+	cout << i++ << ": " << solutionSeven() << " | " << (solutionSeven() == 104743) << endl;
+	cout << i++ << ": " << solutionEight() << " | " << (solutionEight() == 23514624000) << endl;
+	cout << i++ << ": " << solutionNine() << " | " << (solutionNine() == 31875000) << endl;
+	cout << i++ << ": " << solutionTen() << " | " << (solutionTen() == 142913828922) << endl;
+	cout << i++ << ": " << solutionEleven() << " | " << (solutionEleven() == 70600674) << endl;
+	cout << i++ << ": " << solutionTwelve() << " | " << (solutionTwelve() == 76576500) << endl;
+	cout << i++ << ": " << solutionThirteen() << " | " << (solutionThirteen() == 5537376230) << endl;
+	cout << i++ << ": " << solutionFourteen() << " | " << (solutionFourteen() == 837799) << endl;
+	cout << i++ << ": " << solutionFifteen() << " | " << (solutionFifteen() == 137846528820) << endl;
+	cout << i++ << ": " << solutionSixteen() << " | " << (solutionSixteen() == 1366) << endl;
+}
---
+++ TestAnswers.h
@@ -0,0 +1,25 @@
+#ifndef TESTANSWERS_H
+#define TESTANSWERS_H
+
+#include <iostream>
+#include "GenericFunctions.h"
+#include "001\SolutionOne.h"
+#include "002\SolutionTwo.h"
+#include "003\SolutionThree.h"
+#include "004\SolutionFour.h"
+#include "005\SolutionFive.h"
+#include "006\SolutionSix.h"
+#include "007\SolutionSeven.h"
+#include "008\SolutionEight.h"
+#include "009\SolutionNine.h"
+#include "010\SolutionTen.h"
+#include "011\SolutionEleven.h"
+#include "012\SolutionTwelve.h"
+#include "013\SolutionThirteen.h"
+#include "014\SolutionFourteen.h"
+#include "015\SolutionFifteen.h"
+#include "016\SolutionSixteen.h"
+
+void testAnswers();
+
+#endif TESTANSWERS_H
